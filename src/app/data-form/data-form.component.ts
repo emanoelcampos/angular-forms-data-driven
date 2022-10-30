e1b3280fa962eec827cb6a5d48eb9b8cfa1db350
emanoelcampos <emanoel.cv@gmail.com>
2022-10-30 00:12:31 -0300

    Fazendo submit com metodo post

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-data-form',
@@ -8,9 +10,12 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class DataFormComponent implements OnInit {
 
-  formulario!: FormGroup;
+  formulario: FormGroup = new FormGroup({})
 
-  constructor(private  formBuilder: FormBuilder) { }
+  constructor(
+    private  formBuilder: FormBuilder,
+    private httpClient: HttpClient
+    ) { }
 
   ngOnInit(): void {
 
@@ -23,5 +28,15 @@ export class DataFormComponent implements OnInit {
       nome: [null],
       email: [null]
     });
+  }
+
+  onSubmit() {
+    console.log(this.formulario.value);
+
+    this.httpClient.post(
+      'https://httpbin.org/post',
+      JSON.stringify(this.formulario.value))
+    .pipe(map((res: any) => res))
+        .subscribe(dados => console.log(dados));
   }
 }
